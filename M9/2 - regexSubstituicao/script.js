2736cbbd809dfca84bf4f3667a8b1719cb7337d9
JonathanDCarvalho <168686459+JonathanDCarvalho@users.noreply.github.com>
2026-05-05 13:34:47 -0300

    M9

--- M9/2 - regexSubstituicao/script.js
+++ M9/2 - regexSubstituicao/script.js
@@ -37,3 +37,56 @@ console.log("Rafael, Andre".replace(regexpGrupo, "$2 $1"));
 
 console.log("---------------------");
 
+// Mais do que Captura apenas
+// Um grupo também serve para agruparmos uma sequência de caracteres que queremos em repetição.
+
+// Procura: qualquer sequência de ta
+const regexpRep = /(ta)+/gi;
+
+console.log("Tatata, tata, ta".replace(regexpRep, "Pá"));
+// Pá, Pá, Pá
+
+console.log("---------------------");
+
+// Ignorar Captura
+// Utilize o (?:) para ignorar a captura.
+
+// Procura: qualquer sequência de ta
+const regexpIgnore = /(?:ta)+/gi;
+
+console.log("Tatata, tata, ta".replace(regexpIgnore, "Pá"));
+// Pá, Pá, Pá
+
+console.log("---------------------");
+
+// Positive Lookahead
+// Faz a seleção dos itens que possuírem o padrão dentro de (?=) à sua frente. Apesar de utilizar () parênteses o positive lookahead não captura grupo.
+
+// Procura: dígitos em sequência, que
+// possuírem px, sem selecionar o px.
+const regexPositive = /\d(?=px)/g;
+
+console.log("2em, 4px, 5%, 2px, 1px".replace(regexPositive, "X"));
+// 2em, Xpx, 5%, Xpx, Xpx
+console.log("---------------------");
+
+// Negative Lookahead
+// Faz a seleção dos itens não possuírem o padrão dentro de (?!) à sua frente.
+
+// Procura: dígitos que não possuírem px
+// sem selecionar o restante.
+const regexpNegative = /\d(?!px)/g;
+
+console.log("2em, 4px, 5%, 5px, 1px".replace(regexpNegative, "X"));
+// Xem, 4px, X%, 5px, 1px
+console.log("---------------------");
+
+// Positive Lookbehind
+// Faz a seleção dos itens que possuírem o padrão dentro de (?<=) atrás dos mesmos.
+
+// Procura: dígitos que possuírem R$
+// na frente dos mesmos
+const regexpBehind = /(?<=R\$)[\d]+/g;
+
+console.log("R$99, 100, 200, R$20".replace(regexpBehind, "X"));
+// R$X, 100, 200, R$X
---
+++ M9/3 - regexPadroes/script.js
@@ -0,0 +1,85 @@
+// Regexp Padrões
+
+// CEP
+console.log("CEP");
+const regexpCEP = /\d{5}[\s-]?\d{3}/g;
+const ceps = ["00000-000", "00000 000", "00000000"];
+
+for (cep of ceps) {
+	console.log(cep, cep.match(regexpCEP));
+}
+
+console.log("--------------------");
+
+// CPF
+console.log("CPF");
+const regexpCPF = /(?:\d{3}[-.]?){3}\d{2}/;
+
+const cpfs = ["000.000.000-00", "000-000-000-00", "000.000.000.00", "000000000-00", "00000000000"];
+
+for (cpf of cpfs) {
+	console.log(cpf, cpf.match(regexpCPF));
+}
+
+console.log("--------------------");
+
+// CNPJ
+console.log("CNPJ");
+const regexpCNPJ = /\d{2}[-.]?(?:\d{3}[-.]?){2}[/]?\d{4}[-.]?\d{2}/;
+
+const cnpjs = ["00.000.000/0000-00", "00000000000000", "00-000-000-0000-00", "00.000.000/000000", "00.000.000.000000", "00.000.000.0000.00"];
+
+for (cnpj of cnpjs) {
+	console.log(cnpj, cnpj.match(regexpCNPJ));
+}
+
+console.log("--------------------");
+
+// Telefone
+console.log("Telefone");
+const regexpTelefone = /(?:\+?55\s?)?(?:\(?\d{2}\)?)?[-\s]?\d{4,5}[-\s]?\d{4}/;
+
+const telefones = [
+	"+55 11 98888-8888",
+	"+55 11 98888 8888",
+	"+55 11 988888888",
+	"+55 11988888888",
+	"+5511988888888",
+	"5511988888888",
+	"11 98888-8888",
+	"11 98888 8888",
+	"(11) 98888 8888",
+	"(11) 98888-8888",
+	"11-98888-8888",
+	"11 98888 8888",
+	"11988888888",
+	"11988888888",
+	"988888888",
+	"(11)988888888",
+	"98888 8888",
+	"8888 8888",
+];
+
+for (telefone of telefones) {
+	console.log(telefone, telefone.match(regexpTelefone));
+}
+
+console.log("--------------------");
+
+// Email
+console.log("Email");
+
+const regexpEmail = /\d/;
+
+const emails = [
+	"email@email.com",
+	"email@email.com.org",
+	"email-email@email.com",
+	"email_email@email.com",
+	"email.email23@email.com.br",
+	"email.email23@empresa-sua.com.br",
+	"c@contato.cc",
+];
+
+// site de referência pra email regex.
+// http://emailregex.com/
