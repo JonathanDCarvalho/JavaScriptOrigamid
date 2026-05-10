befaa1cb2bcca65dc2eee626bc312e2bfe17a1c8
Jonathan Carvalho <jonathan_dias_carvalho@outlook.com>
2026-05-10 10:04:12 -0300

    adicionado animaisFantasticos Project

---
+++ M11/1 - animaisFantasticos/webpack.config.js
@@ -0,0 +1,30 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+	mode: "development",
+	entry: "./js/script.js",
+	output: {
+		filename: "main.js",
+		path: path.resolve(__dirname),
+	},
+	devtool: "source-map",
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-env"],
+						plugins: ["@babel/plugin-transform-runtime"],
+					},
+				},
+			},
+		],
+	},
+};
